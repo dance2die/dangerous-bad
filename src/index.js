f7af350b57f36c4e292c9450fbfcf166c3667ca5
Sung Kim <sungmkim80@gmail.com>
2019-01-09 16:31:51 -0500

    added react-is

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from "react";
+import domElements from "./domElements";
 
-// https://reactjs.org/docs/dom-elements.html#dangerouslysetinnerhtml
-function DangerousComponent({ html, as: Component, ...rest }) {
-  return <Component dangerouslySetInnerHTML={{ __html: html }} {...rest} />;
-}
+// // https://reactjs.org/docs/dom-elements.html#dangerouslysetinnerhtml
+// function DangerousComponent({ html, as: Component, ...rest }) {
+//   return <Component dangerouslySetInnerHTML={{ __html: html }} {...rest} />;
+// }
 
-const dangerous = html => <DangerousComponent html={html} />;
+// const dangerous = html => <DangerousComponent html={html} />;
+
+// const dangerous =
+
+// refer to https://github.com/styled-components/styled-components/blob/master/src/constructors/styled.js
+domElements.forEach(domElement => {
+  dangerous[domElement] = dangerous(domElement);
+});
+
+export default dangerous;
