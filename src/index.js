325d56601ea33cfe4f822f2345e556df339c4410
Sung Kim <sungmkim80@gmail.com>
2019-01-09 16:21:23 -0500

    Implemented dangerous, Time to implement tag version

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("hi23");
+import React from "react";
+
+// https://reactjs.org/docs/dom-elements.html#dangerouslysetinnerhtml
+function DangerousComponent({ html, as: Component, ...rest }) {
+  return <Component dangerouslySetInnerHTML={{ __html: html }} {...rest} />;
+}
+
+const dangerous = html => <DangerousComponent html={html} />;
